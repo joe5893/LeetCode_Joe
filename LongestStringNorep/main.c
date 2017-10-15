96229a4ae8adab541131028393fe3188081564e8
joe5893 <3137359368@qq.com>
2017-10-15 22:08:42 +0800

    first version

---
+++ LongestStringNorep/main.c
@@ -0,0 +1,81 @@
+/*
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int lengthOfLongestSubstring(char* s) {
+    //char *s = "abcabcbb";
+	int slen = strlen(s);
+	int totlelen = 0;
+	int nextStart=0;
+	int maxlen = 0;
+
+	while(1){
+		int len = lengthOfOneSubstring(s,0,&nextStart);
+		if(len > maxlen){
+			maxlen = len;
+		}
+		s+=nextStart;
+		totlelen += len;		
+		if(totlelen>=slen)
+		{
+			break;
+		}
+	}
+	return maxlen;
+}
+
+int lengthOfOneSubstring(char *s,int start,int *nextStart){
+	int i,j; 
+	int len = 0;
+	int stop = 0;
+	int slen = strlen(s);
+	for(i=start;i<slen;i++){		
+		for(j=start;j<i;j++){//检查有没有重复的字符 
+			if(s[i] == s[j]){
+				stop = 1;
+				break;
+			}
+		}
+		if(stop){
+			break;
+		}
+		else{
+			len++;
+		}
+	} 
+	*nextStart = i;
+	return len;
+}
+
+
+int main(int argc, char *argv[]) {	
+	int maxlen = 0;
+	char *s = "abcabcbb";	
+	
+    maxlen = lengthOfLongestSubstring(s);	
+	printf("%s,%d\n",s,maxlen);
+	
+	s = "bbbbb";		
+    maxlen = lengthOfLongestSubstring(s);	
+	printf("%s,%d\n",s,maxlen);
+	
+	s = "pwwkew";		
+    maxlen = lengthOfLongestSubstring(s);	
+	printf("%s,%d\n",s,maxlen);
+	
+	return 0;
+}
