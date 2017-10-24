68042b8c04ef9eb18e8420914a9353034a40f768
joe5893 <3137359368@qq.com>
2017-10-24 17:26:42 +0800

    the correct version, accepted by Leetcode

--- LongestStringNorep/main.c
+++ LongestStringNorep/main.c
@@ -15,73 +15,64 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int lengthOfLongestSubstring(char* s) {
-    //char *s = "abcabcbb";
-	int slen = strlen(s);
-	int totlelen = 0;
-	int nextStart=0;
-	int maxlen = 0;
 
-	while(1){
-		int len = lengthOfOneSubstring(s,&nextStart);
-		if(len > maxlen){
-			maxlen = len;
-		}
-		s+=1;//nextStart;
-		totlelen += len;		
-		if(totlelen>=slen)
-		{
-			break;
-		}
+//九章算法解答： 
+int lengthOfLongestSubstring(char *s){
+        // 题意为求不包含重复字符的最长子串
+        // left用以记录合法的最远左边界位置，last记录字符上一次出现的位置
+        int ans = 0, left = 0, len = strlen(s),i;//s.length();
+        int last[255];
+        memset(last, -1, sizeof(last));
+        //void *memset(void *s, int ch, size_t n);
+        //函数解释：将s中当前位置后面的n个字节 （typedef unsigned int size_t ）用 ch 替换并返回 s 。
+		//memset：作用是在一段内存块中填充某个给定的值，它是对较大的结构体或数组进行清零操作的一种最快方法
+		
+        for ( i = 0; i < len; i++) {
+            // 上次出现位置在当前记录边界之后，即该子串中出现了重复字符，需调整left使得子串合法
+            if (last[s[i]] >= left) left = last[s[i]] + 1;
+            last[s[i]] = i;
+            ans = max(ans, i - left + 1);
+        }
+        return ans;
+    }
+
+int max(int i, int j){
+	if(i >=j){
+		return i;
 	}
-	return maxlen;
-}
-
-int lengthOfOneSubstring(char *s,int *nextStart){//其实不用nextStart，代码还可以简化 
-	int i,j; 
-	int len = 0;
-	int stop = 0;
-	int slen = strlen(s);
-	
-	//下面用到2重循环 
-	for(i=0;i<slen;i++){		
-		for(j=0;j<i;j++){//检查有没有重复的字符 
-			if(s[i] == s[j]){
-				stop = 1;
-				break;
-			}
-		}
-		if(stop){
-			break;
-		}
-		else{
-			len++;
-		}
-	} 
-	*nextStart = i;
-	return len;
-}
-
-
+	else {
+		return j;
+	}	
+}    
+    
 int main(int argc, char *argv[]) {	
 	int maxlen = 0;
 	char *s = "abcabcbb";	
+	int ans = 3;
 	
     maxlen = lengthOfLongestSubstring(s);	
-	printf("%s,%d\n",s,maxlen);
+	printf("%s, the right answer is %d,  I got %d,   %d\n",s,ans,maxlen,ans==maxlen);
 	
-	s = "dvdf";//"abcabcbb";		
+	s = "wobgrovw";	
+	ans = 6;	
     maxlen = lengthOfLongestSubstring(s);	
-	printf("%s,%d\n",s,maxlen);
+	printf("%s, the right answer is %d,  I got %d,   %d\n",s,ans,maxlen,ans==maxlen);
 	
-	s = "bbbbb";		
-    maxlen = lengthOfLongestSubstring(s);	
-	printf("%s,%d\n",s,maxlen);
 	
-	s = "pwwkew";		
+	s = "dvdf";//"abcabcbb";
+	ans = 3;		
     maxlen = lengthOfLongestSubstring(s);	
-	printf("%s,%d\n",s,maxlen);
+	printf("%s, the right answer is %d,  I got %d,   %d\n",s,ans,maxlen,ans==maxlen);
+	
+	s = "bbbbb";
+	ans = 1;		
+    maxlen = lengthOfLongestSubstring(s);	
+	printf("%s, the right answer is %d,  I got %d,   %d\n",s,ans,maxlen,ans==maxlen);
+	
+	s = "pwwkew";
+	ans = 3;		
+    maxlen = lengthOfLongestSubstring(s);	
+	printf("%s, the right answer is %d,  I got %d,   %d\n",s,ans,maxlen,ans==maxlen);
 	
 	return 0;
 }
